b9e4039609dfc161307043ce68548e1d754a2c3f
Justino4631 <JBaratta3.14159256@hotmail.com>
2025-09-06 14:33:29 -0700

    Initial Choreo Implementation, Untested

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -1,5 +1,8 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
@@ -11,16 +14,23 @@ import swervelib.parser.SwerveParser;
 
 import java.io.File;
 
+import choreo.trajectory.SwerveSample;
+
 public class Swerve extends SubsystemBase {
     private SwerveDrive swerveDrive;
     private boolean initialized = false;
     private Field2d field = new Field2d();
 
+    private final PIDController xController = new PIDController(10.0, 0.0, 0.0);
+    private final PIDController yController = new PIDController(10.0, 0.0, 0.0);
+    private final PIDController headingController = new PIDController(7.5, 0.0, 0.0);
+
     public Swerve() {
         
         try {
-            // On the RIO, deploy files live here:
+            headingController.enableContinuousInput(-Math.PI, Math.PI);
             double maximumSpeed = Units.feetToMeters(4.5);
+            // On the RIO, deploy files live here:
             File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"swerve");
             swerveDrive = new SwerveParser(swerveJsonDirectory).createSwerveDrive(maximumSpeed);
             System.out.println("Swerve is initialized!");
@@ -47,5 +57,23 @@ public class Swerve extends SubsystemBase {
 
     public Field2d getField(){
         return field;
+    }
+
+    public void followTrajectory(SwerveSample sample) {
+        // Get the current pose of the robot
+        Pose2d pose = swerveDrive.getPose();
+
+        // Generate the next speeds for the robot
+        ChassisSpeeds speeds = new ChassisSpeeds(
+            sample.vx + xController.calculate(pose.getX(), sample.x),
+            sample.vy + yController.calculate(pose.getY(), sample.y),
+            sample.omega + headingController.calculate(pose.getRotation().getRadians(), sample.heading)
+        );
+        
+        double xSpeed = speeds.vxMetersPerSecond;
+        double ySpeed = speeds.vyMetersPerSecond;
+        double rotation = speeds.omegaRadiansPerSecond;
+        // Apply the generated speeds
+        drive(xSpeed, ySpeed, rotation, true);
     }
 }
