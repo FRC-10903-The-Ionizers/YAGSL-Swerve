1ba02755338534db13b7275df2e0473bae9a66da
MaxedPC08 <maxedpc@outlook.com>
2025-12-20 10:52:20 -0800

    Made upshift, downshift, toggle field relative in controller. Slight refactor as well.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,6 +2,7 @@ package frc.robot;
 
 import frc.robot.stateSensors.RegionHandler;
 import frc.robot.subsystems.Swerve;
+import frc.robot.subsystems.Vision;
 import frc.robot.util.Controller;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.Filesystem;
@@ -15,41 +16,20 @@ import java.io.File;
 public class RobotContainer {
     private final Swerve swerve = new Swerve();
     private final Controller controller = new Controller(0);
-    private final Controller.SwerveController swerveController = controller.new SwerveController();
     private final RegionHandler regionHandler = new RegionHandler(new File(Filesystem.getDeployDirectory(), "misc/regions.json"));
     private final Vision vision = new Vision(swerve);
 
 
     public RobotContainer() {
         SmartDashboard.putData("SwerveField", swerve.getField());
-        System.out.println(regionHandler.getAllRegionNames());
-
-        Trigger inRegion1 = new Trigger(() -> {
-            boolean isInRegion = regionHandler.inRegion("region1", swerve.getPose());
-            return isInRegion;
-        });
-
-        Trigger inRegion2 = new Trigger(() -> {
-            boolean isInRegion = regionHandler.inRegion("region2", swerve.getPose());
-            return isInRegion;
-        });
-
-        inRegion1.onTrue(Commands.runOnce(() -> System.out.println("Robot entered region1")))
-                 .onFalse(Commands.runOnce(() -> System.out.println("Robot left region1")));
-
-        inRegion2.onTrue(Commands.runOnce(() -> System.out.println("Robot entered region2")))
-                 .onFalse(Commands.runOnce(() -> System.out.println("Robot left region2")));
-
-        inRegion1.and(inRegion2).onTrue(Commands.runOnce(() -> System.out.println("Robot is in both region1 and region2")));
-
     }
 
     public void controllerDrive() {
-        Translation2d driveTranslation = new Translation2d(swerveController.getDriveX(), swerveController.getDriveY());
+        Translation2d driveTranslation = new Translation2d(controller.getDriveX(), controller.getDriveY());
         swerve.getSwerveDrive().drive(
             driveTranslation,
-            swerveController.getRotation(),
-            swerveController.isFieldRelative(),
+            controller.getRotation(),
+            controller.isFieldRelative(),
             false
         );
     }
--- src/main/java/frc/robot/util/Controller.java
+++ src/main/java/frc/robot/util/Controller.java
@@ -1,13 +1,18 @@
 package frc.robot.util;
 
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants;
 
 public class Controller {
     private final XboxController controller;
+    private final CommandXboxController commandController;
+    private double speedMult = 0.6;
 
     public Controller(int port) {
         controller = new XboxController(port);
+        commandController = new CommandXboxController(port);
     }
 
     public double getLeftX() {
@@ -23,37 +28,56 @@ public class Controller {
     }
 
     // -------------------------------
-    // Inner class to manage swerve control
+    // SWERVE CONTROL
     // -------------------------------
-    public class SwerveController {
-        private boolean fieldRelative = true;
+    
+    private boolean fieldRelative = true;
 
-        public void toggleFieldRelative() {
-            fieldRelative = !fieldRelative;
-        }
+    public void toggleFieldRelative() {
+        fieldRelative = !fieldRelative;
+    }
 
-        public boolean isFieldRelative() {
-            return fieldRelative;
-        }
+    public boolean isFieldRelative() {
+        return fieldRelative;
+    }
 
-        /** Get forward/backward, with deadband applied */
-        public double getDriveX() {
-            return applyDeadband(-controller.getLeftY()) * Constants.DriveConstants.kControllerDriveMultiplier;
+    public void upshift(){
+        speedMult+=0.2;
+        if (speedMult > 1){
+            speedMult = 1;
         }
+    }
 
-        /** Get strafe, with deadband applied */
-        public double getDriveY() {
-            return applyDeadband(-controller.getLeftX()) * Constants.DriveConstants.kControllerDriveMultiplier;
+    public void downshift(){
+        speedMult-=0.2;
+        if (speedMult < 0){
+            speedMult = 0;
         }
+    }
 
-        /** Get rotation, with deadband applied */
-        public double getRotation() {
-            return applyDeadband(-controller.getRightX()) * Constants.DriveConstants.kControllerRotationMultiplier;
-        }
+    /** Get forward/backward, with deadband applied */
+    public double getDriveX() {
+        return applyDeadband(-controller.getLeftY()) * Constants.DriveConstants.kControllerDriveMultiplier;
+    }
 
-        private double applyDeadband(double value) {
-            double deadband = 0.1;
-            return Math.abs(value) > deadband ? value : 0.0;
-        }
+    /** Get strafe, with deadband applied */
+    public double getDriveY() {
+        return applyDeadband(-controller.getLeftX()) * Constants.DriveConstants.kControllerDriveMultiplier;
+    }
+
+    /** Get rotation, with deadband applied */
+    public double getRotation() {
+        return applyDeadband(-controller.getRightX()) * Constants.DriveConstants.kControllerRotationMultiplier;
+    }
+
+    private double applyDeadband(double value) {
+        double deadband = 0.1;
+        return Math.abs(value) > deadband ? value : 0.0;
+    }
+
+    public void bind(){
+        commandController.leftStick().onChange(new InstantCommand(this::toggleFieldRelative));
+        commandController.leftBumper().onTrue(new InstantCommand(this::downshift));
+        commandController.rightBumper().onTrue(new InstantCommand(this::upshift));
     }
 }
